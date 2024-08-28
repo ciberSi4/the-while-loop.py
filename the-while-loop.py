8aa5ba7a3d7170ec77ad40e690f4b281fb00c974
ciberSi4 <alex.baikonur@mail.ru>
2024-08-28 19:10:32 +0300

    Add files via upload

---
+++ the-while-loop.py
@@ -0,0 +1,16 @@
+# Стиль кода часть II. Цикл While.
+
+my_list : list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+i : int = 0
+print("Исходные данные:", my_list)
+print("Положительные числа, пока не встретили отрицательное или не закончился список (выход за границу):")
+while i < len(my_list):
+    if my_list[i] == 0:
+        i += 1
+    elif my_list[i] > 0 :
+        print(my_list[i])
+        i += 1
+        continue
+    else:
+        break
